71576f50a24dc3ae7bcc950c303d68eccf0b12e9
k-holy <k.holy74@gmail.com>
2017-09-05 10:54:46 +0900

    Webテスト用スクリプトにコメントを追加、exif_read_data()の警告対策コードを追加

--- tests/web/index.php
+++ tests/web/index.php
@@ -11,10 +11,20 @@ require_once __DIR__ . '/../bootstrap.php';
 
 class Thumbnail
 {
+    /** @var \Volcanus\Thumbnail\Image */
     public $image;
+
+    /** @var string */
     public $title;
+
+    /** @var bool */
     public $callScript = false;
 
+    /**
+     * Thumbnail constructor.
+     *
+     * @param array $props
+     */
     public function __construct($props)
     {
         if (isset($props['image'])) {
@@ -29,14 +39,30 @@ class Thumbnail
     }
 }
 
+/**
+ * @param string $var
+ * @return string
+ */
 function h($var)
 {
     return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
 }
 
+/**
+ * @param string $path
+ * @return mixed
+ */
 function getOrientationFrom($path)
 {
+    $errorLevel = error_reporting();
+    $errorLevelChanged = ($errorLevel & E_WARNING);
+    if ($errorLevelChanged) {
+        error_reporting($errorLevel ^ E_WARNING);
+    }
     $exif = exif_read_data($path);
+    if ($errorLevelChanged) {
+        error_reporting($errorLevel);
+    }
     if (isset($exif['Orientation'])) {
         return $exif['Orientation'];
     }
